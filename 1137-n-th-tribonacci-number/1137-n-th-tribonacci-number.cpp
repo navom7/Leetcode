119df8062408a54953bbebd0fce350ca538893c3
NANHE KUMAR PANDEY <36000529+navom7@users.noreply.github.com>
2024-04-24 07:41:42 +0530

    Time: 2 ms (50.95%), Space: 7.2 MB (29.12%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int> trib(38, 0);
+        trib[1] = 1;
+        trib[2] = 1;
+        for(int i = 3; i <= n; i++) {
+            trib[i] = trib[i-1] + trib[i-2] + trib[i-3];
+        }
+        return trib[n];
+    }
+};
